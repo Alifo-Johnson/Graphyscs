f77c12defbf8d5427453c4ed378c5ad6aed55073
Blue Moon <83472423+Alifo-Johnson@users.noreply.github.com>
2025-10-08 11:53:25 +0100

    Made Changes on matplotlib

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 from scipy.stats import linregress
 import io # Required for the download button
 
